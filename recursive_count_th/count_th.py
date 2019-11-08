9f8ae013b93b63bc282d56c56cd5b045e1bb7f0d
Rory Murray <rory.christian.murray@gmail.com>
2019-11-08 12:30:10 -0500

    first pass

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -10,6 +10,11 @@ def count_th(word):
     # TBC
     # recursive call
 
+    # how would I do this
+    # scan through and look for occurrences where
+    # word[i] + word[i + 1] = "th"
+    # how to keep track of count
+
     # return
     # what do we return
     # we return a count of string occurrences
@@ -17,10 +22,23 @@ def count_th(word):
 
     # base case
     # what do we drive to
-
-    # how would I do this
-    # scan through and look for occurrences where
-    # word[i] + word[i + 1] = "th"
-    # how to keep track of count
+    print(f"word : {word[0:2]}")
+    print(f"len(word) : {len(word)}")
+    print(f"word[0] : {word[0]}")
+    if len(word) = 0:
+        print("fart")
+        return 0
+    elif word[0] != "t":
+        print(f"\nword before != t : {word}\n")
+        recword = word[1::]
+        print(f"\nrecword after != t : {recword}\n")
+        return count_th(word[1::])
+    elif word[0] + word[1] == "th":
+        print(f"\nword before th : {word}\n")
+        recword = word[2::]
+        print(f"\nbase : {base}\n")
+        # base = base + 1
+        print(f"\nrecword after th : {recword}\n")
+        return count_th(word[2::])
 
     pass
